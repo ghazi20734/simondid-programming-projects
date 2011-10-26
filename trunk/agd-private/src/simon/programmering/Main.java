39451a7cbf1c00f9a0a44736bdd0e1ee984bf0af
elcokker <elcokker@0978b2e5-a365-4210-1360-154ff6aefd8e>
2011-10-26 18:02:36 +0000

    der er kommet farver på trekanten

--- trunk/agd-private/src/simon/programmering/Main.java
+++ trunk/agd-private/src/simon/programmering/Main.java
@@ -21,6 +21,7 @@ public class Main extends GameActivity implements GameListener {
     /** Called when the activity is first created. */
     
     private FloatBuffer vertices;
+    private FloatBuffer colors;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setGameListener(this);
@@ -31,8 +32,8 @@ public class Main extends GameActivity implements GameListener {
 		 ByteBuffer buffer = ByteBuffer.allocateDirect( 3 * 4 * 3 );
 	       buffer.order(ByteOrder.nativeOrder());
 	       vertices = buffer.asFloatBuffer();
-	       vertices.put( -0.5f );
-	       vertices.put( -0.5f );
+	       vertices.put( -0.6f );
+	       vertices.put( -0.4f );
 	       vertices.put( 0 );
 	       vertices.put( 0.5f );
 	       vertices.put( -0.5f );
@@ -41,6 +42,22 @@ public class Main extends GameActivity implements GameListener {
 	       vertices.put( 0.5f );
 	       vertices.put( 0 );	
 	       vertices.rewind();
+	       buffer = ByteBuffer.allocateDirect( 3 * 4 * 4 );
+	       buffer.order(ByteOrder.nativeOrder());
+	       colors = buffer.asFloatBuffer();	
+	       colors.put( 1 );
+	       colors.put( 0 );
+	       colors.put( 0 );
+	       colors.put( 1 );
+	       colors.put( 0 );
+	       colors.put( 1 );
+	       colors.put( 0 );
+	       colors.put( 1 );	
+	       colors.put( 0 );
+	       colors.put( 0 );
+	       colors.put( 1 );
+	       colors.put( 1 );	
+	       colors.rewind();
 	}
 
 	public void mainLoopIteration(GameActivity activity, GL10 gl) {
@@ -49,5 +66,7 @@ public class Main extends GameActivity implements GameListener {
 	       gl.glEnableClientState(GL10.GL_VERTEX_ARRAY );    
 	       gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertices);
 	       gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
+	       gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+	       gl.glColorPointer(4, GL10.GL_FLOAT, 0, colors);
 	}
 }
